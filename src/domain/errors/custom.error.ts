9d7e36486fb363bdaddbf6337b5fdd81985acc89
GalvisDev <galvisdeveloper@gmail.com>
2024-08-10 12:14:41 -0500

    Manejo de errores predefinidos y dinamicos

---
+++ src/domain/errors/custom.error.ts
@@ -0,0 +1,41 @@
+
+
+
+export class CustomError extends Error {
+
+    private constructor(
+        public readonly code: number,
+        public readonly message: string,
+    ) {
+        super(message);
+    }
+
+    static badRequest(message: string) {
+        return new CustomError(400, message);
+    }
+
+    static unauthorized(message: string) {
+        return new CustomError(401, message);
+    }
+
+    static forbidden(message: string) {
+        return new CustomError(403, message);
+    }
+
+    static notFound(message: string) {
+        return new CustomError(404, message);
+    }
+
+    static conflict(message: string) {
+        return new CustomError(409, message);
+    }
+
+    static internal(error: Error | string) {
+        return new CustomError(500, error instanceof Error ? error.message : error);
+    }
+
+    static fromCode(code: number, message: string) {
+        return new CustomError(code, message);
+    }
+
+}
